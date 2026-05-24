dab37f5cc6491d71e5d378e745c1a146a6e4d728
achiniThes <achini.thesanya@gmail.com>
2026-05-24 15:02:20 +0530

    Update calculator methods

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -4,13 +4,28 @@ public class CalculatorTest {
 
         Calculator cal=new Calculator();
 
-        if(cal.add(2,3)==5){
+        if (cal.add(2, 3) == 5) {
+            System.out.println("Addition Test Passed");
+        } else {
+            System.out.println("Addition Test Failed");
+        }
 
-            System.out.println("Test Passed");
+        if (cal.subtract(5, 2) == 3) {
+            System.out.println("Subtraction Test Passed");
+        } else {
+            System.out.println("Subtraction Test Failed");
+        }
 
-        }else{
+        if (cal.multiply(4, 3) == 12) {
+            System.out.println("Multiplication Test Passed");
+        } else {
+            System.out.println("Multiplication Test Failed");
+        }
 
-            System.out.println("Test Failed");
+        if (cal.divide(10, 2) == 5) {
+            System.out.println("Division Test Passed");
+        } else {
+            System.out.println("Division Test Failed");
         }
     }
     
